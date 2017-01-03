98c9f50ac8e5803236861cb292d084209042bf45
nickyprabowo <prabowonicky@gmail.com>
2017-01-03 10:53:34 +0700

    Adding typography customizer

--- wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -3,3 +3,5 @@
 {"user_id":1,"user_login":"mimin","user_remoteaddr":"127.0.0.1","user_hostname":"activate.adobe.com","user_lastlogin":"2016-12-30 08:26:16"}
 {"user_id":1,"user_login":"mimin","user_remoteaddr":"127.0.0.1","user_hostname":"activate.adobe.com","user_lastlogin":"2016-12-30 10:55:37"}
 {"user_id":1,"user_login":"mimin","user_remoteaddr":"127.0.0.1","user_hostname":"activate.adobe.com","user_lastlogin":"2016-12-30 17:32:56"}
+{"user_id":1,"user_login":"mimin","user_remoteaddr":"127.0.0.1","user_hostname":"activate.adobe.com","user_lastlogin":"2017-01-02 18:42:56"}
+{"user_id":1,"user_login":"mimin","user_remoteaddr":"127.0.0.1","user_hostname":"activate.adobe.com","user_lastlogin":"2017-01-03 09:53:22"}
