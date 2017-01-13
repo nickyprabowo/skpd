82cf62f929ad14ca43fe413b665055601c6f9aa9
Vive Vio Permana <vive.permana@gmail.com>
2017-01-13 09:44:01 +0700

    Update header picker

---
+++ wp-content/themes/SKPD/framework-customizations/theme-functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Vive Vio Permana <vive.permana@gmail.com>
+ *
+ * This file is inital or setter and getter for plugin uniyson
+ * 
+ */
+
+/**
+ * Get header style from the chosen in admin
+ * @return html
+ */
+function fw_theme_get_header_style() {
+
+    if ( ! defined('FW') ) return; // prevent fatal error when the framework is not active
+
+    $header_file = fw_get_db_settings_option('jsc_theme_header_style');
+
+    include( get_template_directory() . '/framework-customizations/header-style/' . $header_file . '.php' );
+   
+}
